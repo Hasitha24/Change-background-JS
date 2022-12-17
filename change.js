24f4ef58dd5c576d0b00f971c89453e9cc29a8ce
Hasitha24 <60382863+Hasitha24@users.noreply.github.com>
2022-12-18 00:04:35 +0530

    Add files via upload

---
+++ change.js
@@ -0,0 +1,16 @@
+
+   // const btn =document.getElementById('clickme')onclick.addEventListener ('clickme', function onclick(event)){
+
+  //  });
+  //document.getElementById("clickme").onclick.body.style.backgroundColor = 'pink';
+   // document.body.style.backgroundColor ='pink';
+
+
+   const clickme = document.getElementById('clickme');
+
+   clickme.addEventListener('click', function onClick(event) {
+    document.body.style.backgroundColor = 'black';
+
+   });
+
+
